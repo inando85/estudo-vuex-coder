4bcf177a4c1ff10c4c82a6d606aec4d2a058e034
Fernando Junior <fernando.junior85@outlook.com>
2021-07-28 16:22:53 -0300

    Projeto Finalizado

---
+++ src/store/modules/carrinho.js
@@ -0,0 +1,25 @@
+export default {
+  namespaced: true,
+  state: {
+    produtos: []
+  },
+  getters: {
+    valorTotal(state) {
+      return state.produtos.map((p) => p.quantidade * p.preco)
+        .reduce((total, atual) => total + atual, 0)
+    },
+    produtosTotal(state) {
+      return state.produtos
+    }
+  },
+  mutations: {
+    adicionarProduto(state, payload) {
+      state.produtos.push(payload)
+    }
+  },
+  actions: {
+    adicionarProduto({ commit}, payload) {
+      commit('adicionarProduto', payload)
+    }
+  }
+}
